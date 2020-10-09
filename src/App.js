07e4e11e03ec8e6228bbb683ca503ba2819df675
April Darger <april.m.darger@gmail.com>
2020-10-09 09:48:30 -0600

    Setting up App function structure and Character.js basic structure before api call.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [StarData, setStarData] = useState()
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    const fetchStarData = () => {
+      axios.get(``)
+        .then(res => {
+          setStarData(res.data)
+        })
+        .catch(err => {
+          debugger
+        })
+    }
+    fetchStarData()
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react'
+
+
+function Character(props) {
+    const { } = props
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Character;
